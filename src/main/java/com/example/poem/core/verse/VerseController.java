98652e1eec4ec9e5dbbb8e20fc848b2c8f1d8e69
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-04-18 21:39:46 +0200

    feat: verse detailsPage

--- src/main/java/com/example/poem/core/verse/VerseController.java
+++ src/main/java/com/example/poem/core/verse/VerseController.java
@@ -1,35 +1,29 @@
 package com.example.poem.core.verse;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/verse")
 public class VerseController {
 
-  static List<Verse> verseList = List.of(
-      Verse.builder().text(
-              " \"May the God of hope fill you with all joy and peace as you trust in him, so that you may overflow with hope by the power of the Holy Spirit.\" - Romans 15:13")
-          .title("verse1")
-          .shortDescription("Short desc1")
-          .imageUrl("images/zdjecie7.jpg")
-          .build(),
-      Verse.builder().text(
-              " \"May the God of hope fill you with all joy and peace as you trust in him, so that you may overflow with hope by the power of the Holy Spirit.\" - Romans 15:13")
-          .title("verse2")
-          .shortDescription("Short desc1")
-          .imageUrl("images/zdjecie7.jpg")
-          .build()
-  );
+  private final VerseRepository repository;
 
   @GetMapping
   public String getAllVerses(Model model) {
-    System.out.println("getAllVerses()");
-    model.addAttribute("verses", verseList);
+    model.addAttribute("verses", repository.findAll());
     return "verse/allVerses";
+  }
+
+  @GetMapping("{id}")
+  public String getVerse(Model model, @PathVariable Long id) {
+    Verse verse = repository.findById(id).orElseThrow();
+    model.addAttribute("verse", verse);
+    return "verse/verseDetails";
   }
 }
